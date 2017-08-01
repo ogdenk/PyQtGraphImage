2ad9e2d9ec678f1c8ac86a3f50e04bdbb5b8b8ea
ogdenk <ogdenk@upstate.edu>
2017-08-01 15:33:30 -0400

    A lot happened

--- MainWindow.py
+++ MainWindow.py
@@ -18,19 +18,51 @@ class PyQtGraphImageMain(QMainWindow, ui_PyQtGraphImage.Ui_MainWindow):
         self.imv = pqg.ImageView(parent=self.graphicsView)
         #self.imv.show()
         ChestCT = dicom.read_file("ch_250.dcm")
+        print("rescale intercept \n")
+        print(ChestCT[0x28,0x1052].value)
         ConstPixelDims = (int(ChestCT.Rows), int(ChestCT.Columns))
         ConstPixelSpacing = (float(ChestCT.PixelSpacing[0]), float(ChestCT.PixelSpacing[1]))
+        out = ""
         print(ConstPixelDims)
         print(ConstPixelSpacing)
         print(ChestCT.pixel_array.dtype)
+        out += "Pixel Dimentions: " + ConstPixelDims.__str__() + '\n' + "Pixel Spacing: " + ConstPixelSpacing.__str__() + '\n'
 
         ArrayDicom = np.zeros(ConstPixelDims, dtype=ChestCT.pixel_array.dtype)
-        ArrayDicom[:, :] = ChestCT.pixel_array
+        ArrayDicom[:, :] = ChestCT.pixel_array+ChestCT[0x28,0x1052].value
 
         self.imv.setImage(ArrayDicom.T)
+        self.imv.autoRange()
 
 
-        #self.apply.clicked.connect(self.ApplyChecker)
-        #self.reset.clicked.connect(self.Reset)
-        #self.patient = PatientData.Patient() #object that holds the data and does the calculations
+        #Creates the ROI
+        roi = pqg.RectROI([250,250], [150,150])
+        roi.setParentItem(self.imv.getView())
+        #roi.setAngle(-90)
+        #roi.setPos(100,100)
+
+
+        #Generates second image and output from ROI data
+        img2 = pqg.ImageView(self.graphicsView_2)
+        ROIarray = roi.getArrayRegion(ArrayDicom, self.imv.getImageItem())
+        print(ROIarray)
+        np.fliplr(ROIarray)
+        out += "ROI array:\n" + ROIarray.__str__()
+        img2.setImage(ROIarray.T, autoLevels=False, autoRange=False, scale=[0.5, 0.5])
+        img2.setLevels(ROIarray.min(),ROIarray.max())
+
+        def update(roi):
+            ROIarray = roi.getArrayRegion(ArrayDicom.T, self.imv.getImageItem())
+            np.fliplr(ROIarray)
+            img2.setImage(ROIarray, autoRange=False, autoLevels=False, scale=[0.5, 0.5])
+            print(ROIarray)
+            print(ROIarray.mean())
+            print(ROIarray.max())
+            print(ROIarray.min())
+            updatetext = ''
+            updatetext += ROIarray.__str__() + "\nMean:\n" + ROIarray.mean().__str__() + "\nMax:\n" + ROIarray.max().__str__() + "\nMin:\n" + ROIarray.min().__str__()
+            self.textBrowser.clear()
+            self.textBrowser.setPlainText(out + updatetext)
+
+        roi.sigRegionChanged.connect(update)
 
--- ui_PyQtGraphImage.py
+++ ui_PyQtGraphImage.py
@@ -17,6 +17,23 @@ class Ui_MainWindow(object):
         self.graphicsView = PlotWidget(self.centralwidget)
         self.graphicsView.setGeometry(QtCore.QRect(90, 100, 512, 512))
         self.graphicsView.setObjectName("graphicsView")
+        self.graphicsView_2 = QtWidgets.QGraphicsView(self.centralwidget)
+        self.graphicsView_2.setGeometry(QtCore.QRect(650, 100, 261, 261))
+        self.graphicsView_2.setObjectName("graphicsView_2")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(100, 70, 81, 16))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(660, 70, 71, 16))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(660, 400, 60, 16))
+        self.label_3.setObjectName("label_3")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(650, 430, 256, 181))
+        self.textBrowser.setMaximumSize(QtCore.QSize(500, 500))
+        self.textBrowser.setTextInteractionFlags(QtCore.Qt.TextSelectableByKeyboard|QtCore.Qt.TextSelectableByMouse)
+        self.textBrowser.setObjectName("textBrowser")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1024, 22))
@@ -32,5 +49,8 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Main Window"))
+        self.label_2.setText(_translate("MainWindow", "ROI Display"))
+        self.label_3.setText(_translate("MainWindow", "Output"))
 
 from pyqtgraph import PlotWidget
